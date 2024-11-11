668dc58158da85bea94f547de3c649c660a77dd3
vishnuu123pl <137207623+vishnuu123pl@users.noreply.github.com>
2024-11-11 18:09:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -66,7 +66,7 @@ PAYMENT_TEXT = environ.get('PAYMENT_TEXT', '<b>- ᴀᴠᴀɪʟᴀʙʟᴇ ᴘʟ�
 OWNER_USERNAME = environ.get('OWNER_USERNAME', 'vis_hnu_bot') # owner username without @
 
 # Clone Information : If Clone Mode Is True Then Bot Clone Other Bots.
-CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
+CLONE_MODE = bool(environ.get('CLONE_MODE', False)) # Set True or False
 CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "mongodb+srv://V:V@cluster0.ac4xjmt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Necessary If clone mode is true
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'asianet_surya_zee') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
